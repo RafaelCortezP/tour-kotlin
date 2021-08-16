0250a22a1d0d4fb3da9049c3b0c04d5fbb51c1cd
Rafael Cortez <rafaelcortezp@gmail.com>
2021-08-16 20:29:20 -0300

    Parameters

--- src/main/kotlin/com/rafaelcortez/tour/controller/PromocaoController.kt
+++ src/main/kotlin/com/rafaelcortez/tour/controller/PromocaoController.kt
@@ -2,6 +2,7 @@ package com.rafaelcortez.tour.controller
 
 import com.rafaelcortez.tour.model.Promocao
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -18,7 +19,7 @@ class PromocaoController {
         return "Hello world"
     }
 
-    @RequestMapping(value = ["/promocoes"], method = arrayOf(RequestMethod.GET))
-    fun getPromocao() = promocoes[2]
+    @RequestMapping(value = ["/promocoes/{id}"], method = arrayOf(RequestMethod.GET))
+    fun getPromocao(@PathVariable id: Long) = promocoes[id]
 
 }
